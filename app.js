c0c3353a0b51f581379b738de4e037ef52048088
Rajeesh CV <rajeesh.cv@devfactory.com>
2019-01-17 11:58:50 +0530

    Listen on all interfaces

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello DevSpace from ExpressJS!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, '0.0.0.0', () => console.log(`Example app listening on port ${port}!`))
